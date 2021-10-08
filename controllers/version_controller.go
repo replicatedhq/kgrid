3a08bab6e4af0bfaf438dee86c6bea62a2919de3
divolgin <dmitriy@replicated.com>
2021-10-08 17:45:47 +0000

    Fix RBAC

--- controllers/version_controller.go
+++ controllers/version_controller.go
@@ -45,6 +45,8 @@ type VersionReconciler struct {
 
 //+kubebuilder:rbac:groups="",namespace=kgrid-system,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
+//+kubebuilder:rbac:groups="coordination.k8s.io",namespace=kgrid-system,resources=leases,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
